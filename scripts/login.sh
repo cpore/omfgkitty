7a87a84ba068a4909cc9e96619cb46c795780e65
Casey Pore <cpore@PORE2.americas.hpqcorp.net>
2016-11-11 00:39:31 -0700

    stuff

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ssh -L 30143:columbus-oh.cs.colostate.edu:30143 -L 30154:denver.cs.colostate.edu:30154 -L 30153:denver.cs.colostate.edu:30153 -L 30156:columbus-oh.cs.colostate.edu:30156 cpore@columbus-oh.cs.colostate.edu
+ssh -L 30156:columbus-oh.cs.colostate.edu:30156 -L 30155:denver.cs.colostate.edu:30155 cpore@columbus-oh.cs.colostate.edu
