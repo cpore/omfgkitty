127bcbddcb02ff1f1f017eb8b38a4523115b06c4
Casey Pore <caseypore@gmail.com>
2016-11-13 23:02:54 -0700

    test script

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import pyspark
 import socket
 from time import sleep
+import sys
+import cv2
 
 
 def print_hostname(LOGGER):
-    i = 0
-    while True:
-        LOGGER.info('iteration:', i, socket.gethostname())
+    for i in range(30):
+        LOGGER.info("-----------------------------------test info---------------------------------")
+        LOGGER.info('iteration:' + str(i))
+        LOGGER.info('host:' + socket.gethostname())
+        LOGGER.info('interpreter version:' + sys.version)
+        LOGGER.info('openCV version:' + cv2.__version__)
         sleep(10)
-        i = i + 1
         
 if __name__ == '__main__':
     sc = pyspark.SparkContext()
@@ -17,3 +21,7 @@ if __name__ == '__main__':
     log4jLogger = sc._jvm.org.apache.log4j
     LOGGER = log4jLogger.LogManager.getLogger(__name__)
     LOGGER.info("pyspark script logger initialized")
+    print_hostname(LOGGER)
+    
+    
+#$SPARK_HOME/bin/spark-submit --py-files test.py --deploy-mode cluster --master yarn --supervise 
