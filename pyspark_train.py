c9bef22f318e77ad6541852a2fde3a47e31b83b4
Casey Pore <caseypore@gmail.com>
2016-11-21 00:20:26 -0700

    add pmml save

--- pyspark_train.py
+++ pyspark_train.py
@@ -21,7 +21,7 @@ def train():
     print("-----------------------------------------Training Error = " + str(trainErr) + "----------------------------------------------")
     
     # Save and load model
-    model.save(sc, "hdfs://columbus-oh.cs.colostate.edu:30148/model")
+    model.overwrite(sc, "hdfs://columbus-oh.cs.colostate.edu:30148/model")
     model.toPMML(sc, "hdfs://columbus-oh.cs.colostate.edu:30148/model/model.xml")
     #sameModel = SVMModel.load(sc, "hdfs://columbus-oh.cs.colostate.edu:30148/model/model")
     
